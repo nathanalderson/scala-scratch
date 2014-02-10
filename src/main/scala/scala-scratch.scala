94cb78d038a03b784df9d282c98f289934227c66
Nathan Alderson <nathan.alderson@gmail.com>
2014-02-10 15:45:19 -0600

    working

--- src/main/scala/scala-scratch.scala
+++ src/main/scala/scala-scratch.scala
@@ -1,17 +1,69 @@
-package src.main.scala
 
 object GameOfLife {
-    def get_neighbors(x: Int, y: Int) = {
-        for (dx <- List(-1,0,1); dy <- List(-1,0,1) if !(dx==0 && dy==0))
-            yield (x+dx, y+dy)
+    type Point = Tuple2[Int, Int]
+    type Board = Set[Point]
+
+    def get_neighbors(p: Point) = {
+        for (dx <- Set(-1,0,1); dy <- Set(-1,0,1) if !(dx==0 && dy==0))
+            yield new Point(p._1+dx, p._2+dy)
     }
 
-    def main(args: Array[String]) = {
-        val glider          = ((2, 0), (2, 1), (2, 2), (1, 2), (0, 1))
-        val light_spaceship = ((2, 0), (4, 0), (1, 1), (1, 2), (1, 3), (4, 3), (1, 4), (2, 4), (3, 4))
-        val blinker         = ((1,0), (1,1), (1,2))
+    def make_stepper(birth_num: List[Int], survive_num: List[Int])
+        : Board => Board =
+    {
+        return (board) => {
+            var all_neighbors = board.toList.flatMap(get_neighbors)
+            var histogram = all_neighbors.groupBy(identity).mapValues(_.size)
+            for ( (loc,n) <- histogram.toSet if (board.contains(loc) && survive_num.contains(n))
+                                              || birth_num.contains(n) )
+                yield loc
+        }
+    }
 
-        val cells = glider
+    def make_drawer(width: Int, height: Int)
+        : Board => String =
+    {
+        return (board: Board) => {
+            val s = new StringBuilder
+            for (y <- 0 until height) {
+                for (x <- 0 until width) {
+                    if (board.contains((x,y)))
+                        s ++= "x"
+                    else
+                        s ++= " "
+                }
+                s ++= "|\n"
+            }
+            s ++= "-"*width
+            s.toString()
+        }
+    }
+
+    val step = make_stepper(List(3), List(2,3))
+    val draw = make_drawer(40, 10)
+
+    def main(args: Array[String]) = {
+        val glider          = Set((2, 0), (2, 1), (2, 2), (1, 2), (0, 1))
+        val light_spaceship = Set((2, 0), (4, 0), (1, 1), (1, 2), (1, 3), (4, 3), (1, 4), (2, 4), (3, 4))
+        val blinker         = Set((1,0), (1,1), (1,2))
+
+        var board: Board = blinker
+
+        def run_and_print_each(board: Board) {
+            println(draw(board))
+            Thread.sleep(200)
+            run_and_print_each(step(board))
+        }
+
+        def run_until(board: Board, generations: Int) {
+            if (generations == 0)
+                println(draw(board))
+            else
+                run_until(step(board), generations-1)
+        }
+
+        // run_until(board, 10000)
+        run_and_print_each(board)
     }
 }
 
--- src/test/scala/test-scala-scratch.scala
+++ src/test/scala/test-scala-scratch.scala
@@ -1,6 +1,6 @@
-package src.main.scala
 
 import org.scalatest._
+import GameOfLife._
 
 abstract class UnitSpec extends FunSpec
 
@@ -8,43 +8,94 @@ class GameOfLifeSpec extends UnitSpec {
 
     describe("get_neighbors") {
 
-        def compare(neighbors: List[(Int, Int)], expected: List[(Int, Int)]) {
-            assert(neighbors.length == 8)
-            assert(neighbors.sorted == expected.sorted)
+        def compare(neighbors: Board, expected: Board) {
+            assert(neighbors.size == 8)
+            assert(neighbors == expected)
         }
 
         describe("for origin") {
             it("should return correct values") {
-                val neighbors = GameOfLife.get_neighbors(0,0)
-                val expected_neighbors = List((-1, -1), (-1, 0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1))
+                val neighbors = get_neighbors(0,0)
+                val expected_neighbors = Set((-1, -1), (-1, 0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1))
                 compare(neighbors, expected_neighbors)
             }
         }
 
         describe("for points with positive x,y coordinates") {
             it("should return correct values") {
-                val neighbors = GameOfLife.get_neighbors(1,1)
-                val expected_neighbors = List((0,0), (0,1), (0,2), (1,2), (1,0), (2,0), (2,1), (2,2))
+                val neighbors = get_neighbors(1,1)
+                val expected_neighbors = Set((0,0), (0,1), (0,2), (1,2), (1,0), (2,0), (2,1), (2,2))
                 compare(neighbors, expected_neighbors)
             }
         }
 
         describe("for points with negative x,y coordinates") {
             it("should return correct values") {
-                val neighbors = GameOfLife.get_neighbors(-1,-1)
-                val expected_neighbors = List((0,0), (0,-1), (0,-2), (-1,-2), (-1,0), (-2,-0), (-2,-1), (-2,-2))
+                val neighbors = get_neighbors(-1,-1)
+                val expected_neighbors = Set((0,0), (0,-1), (0,-2), (-1,-2), (-1,0), (-2,-0), (-2,-1), (-2,-2))
                 compare(neighbors, expected_neighbors)
             }
         }
 
         describe("for points with big values") {
             it("should *still* return correct values") {
-                val neighbors = GameOfLife.get_neighbors(300,123)
-                val expected_neighbors = List((301, 122), (301, 123), (301, 124),
+                val neighbors = get_neighbors(300,123)
+                val expected_neighbors = Set((301, 122), (301, 123), (301, 124),
                                               (300, 122),             (300, 124),
                                               (299, 122), (299, 123), (299, 124))
                 compare(neighbors, expected_neighbors)
             }
+        }
+    } // describe("get_neighbors")
+
+    describe("step") {
+        describe("for rule 1") {
+            it("should kill cells with no neighbors") {
+                val board = Set((0,0))
+                assert(!step(board).contains((0,0)))
+            }
+            it("should kill cells with only one neighbor") {
+                val board = Set((0,0), (0,1))
+                assert(!step(board).contains((0,0)))
+            }
+        }
+        describe("for rule 2") {
+            it("should persist cells with two neighbors") {
+                val board = Set((0,0), (0,1), (0,2))
+                assert(step(board).contains((0,1)))
+            }
+        }
+        describe("for rule 3") {
+            it("should kill cells with 4 neighbors") {
+                val board = Set((0,0), (0,1), (0,2), (1,1), (-1, 1))
+                assert(!step(board).contains((0,1)))
+            }
+        }
+        describe("for rule 4") {
+            it("should birth dead cells with 3 neighbors") {
+                val board = Set((0,0), (1,1), (0,2))
+                assert(step(board).contains((0,1)))
+            }
+        }
+    }
+
+    describe("draw") {
+        it("should draw an empty board") {
+            val draw = make_drawer(3,4)
+            assert( "   |\n   |\n   |\n   |\n---" == draw(Set()) )
+        }
+        it("should draw some cells") {
+            val draw = make_drawer(3,4)
+            assert( "  x|\n x |\n   |\n   |\n---" == draw(Set((1,1), (2,0))) )
+        }
+    }
+
+    describe("blinker") {
+        it("should alternate between states") {
+            val blinker1 = Set((-1,0), (0,0), (1,0))
+            val blinker2 = Set((0,-1), (0,0), (0,1))
+            assert (step(blinker1) == blinker2)
+            assert (step(blinker2) == blinker1)
         }
     }
 }
